9001d0b737ea57d4f0366459c13b08b423239795
seorinnn <srgongju@naver.com>
2024-10-26 23:47:48 +0900

    feat : 일반회원이 트레이너회원 경력 조회하는 getTrainerCareer api 함수 정의

--- src/api/career/getCareer.ts
+++ src/api/career/getCareer.ts
@@ -1,8 +1,17 @@
 import { fetchAPI } from '..';
 
+// 트레이너회원 본인이 경력 조회 시
 export const getCareer = async () => {
   return await fetchAPI({
     method: 'GET',
     endpoint: '/careers',
+  });
+};
+
+// 일반회원이 트레이너회원 경력 조회 시
+export const getTrainerCareer = async (trainerId: number) => {
+  return await fetchAPI({
+    method: 'GET',
+    endpoint: `trainers/${trainerId}`,
   });
 };
