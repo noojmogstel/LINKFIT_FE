83076cdb72216446aeb228150cc73d15c251dfbc
seorinnn <srgongju@naver.com>
2024-10-28 13:42:50 +0900

    feat : useProfile 커스텀훅에 일반회원이 트레이너회원 프로필 조회하는 로직 추가

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,24 +1,34 @@
 import { useEffect, useState } from 'react';
 
-import { getTrainerProfile } from '@/api/profile/getTrainerProfile';
+import {
+  getTrainerProfile,
+  getTrainerProfileForUser,
+} from '@/api/profile/getTrainerProfile';
 import { getUserProfile } from '@/api/profile/getUserProfile';
 import { useAuth } from '@/hooks/useAuth';
-import { TrainerProfile, UserProfile } from '@/types';
+import { TrainerProfile, TrainerProfileForUser, UserProfile } from '@/types';
 
-export const useProfile = () => {
+export const useProfile = (trainerId?: number) => {
   const { type } = useAuth();
-  const [profile, setProfile] = useState<UserProfile | TrainerProfile | null>(
-    null
-  );
+  const [profile, setProfile] = useState<
+    TrainerProfileForUser | UserProfile | TrainerProfile | null
+  >(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        if (type === 'user') {
-          const data = await getUserProfile();
+        if (trainerId && type === 'user') {
+          // 일반회원이 특정 트레이너의 프로필을 조회할 때
+          const data: TrainerProfileForUser =
+            await getTrainerProfileForUser(trainerId);
+          setProfile(data);
+        } else if (type === 'user') {
+          // 일반회원이 본인의 프로필을 조회할 때
+          const data: UserProfile = await getUserProfile();
           setProfile(data);
         } else if (type === 'trainer') {
-          const data = await getTrainerProfile();
+          // 트레이너가 본인의 프로필을 조회할 때
+          const data: TrainerProfile = await getTrainerProfile();
           setProfile(data);
         }
       } catch (error) {
