c1ac8d3ff27b632469d6e24604b416d1f9dd1e95
seorinnn <srgongju@naver.com>
2024-10-28 13:47:52 +0900

    style : TrainerProfile 컴포넌트 스타일 정의

---
+++ src/components/Proposal/TrainerDetail/TrainerProfile.styles.ts
@@ -0,0 +1,31 @@
+import { CardBody, Image, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  margin: 30px 0 20px;
+`;
+
+export const StyledCardBody = styled(CardBody)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+`;
+
+export const StyledProfileImage = styled(Image)`
+  width: 80px;
+  border: 1px solid #eeeeee;
+  border-radius: 100%;
+`;
+
+export const StyledNameText = styled(Text)`
+  font-weight: 800;
+  font-size: 20px;
+`;
+
+export const StyledGymText = styled(Text)`
+  font-weight: 500;
+  font-size: 17px;
+`;
