0c75ad31a9581ea9f0a63f0772914038006c8b4e
seorinnn <srgongju@naver.com>
2024-11-01 15:12:00 +0900

    style : PT 매칭된 트레이너 프로필 스타일 정의

---
+++ src/components/PtSchedule/TrainerProfile.styles.ts
@@ -0,0 +1,46 @@
+import { colors } from '@/styles/colors';
+import { Button, CardBody, Image, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  margin: 30px 0 20px;
+`;
+
+export const StyledCardBody = styled(CardBody)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+`;
+
+export const StyledProfileImage = styled(Image)`
+  width: 80px;
+  border: 1px solid #eeeeee;
+  border-radius: 100%;
+  margin-top: 10px;
+  margin-right: 20px;
+`;
+
+export const StyledNameText = styled(Text)`
+  font-weight: 800;
+  font-size: 20px;
+`;
+
+export const StyledGymText = styled(Text)`
+  font-weight: 500;
+  font-size: 14px;
+  margin-top: 8px;
+`;
+
+export const StyledChatButton = styled(Button)`
+  background-color: ${colors.mainColor};
+  color: ${colors.white};
+  &:hover {
+    background-color: #ff467e;
+  }
+  width: 80%;
+  height: 30px;
+  margin-top: 12px;
+`;
