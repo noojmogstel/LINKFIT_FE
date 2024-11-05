2d8b3e36216229732cba6a000c715120a7a82f45
seorinnn <srgongju@naver.com>
2024-11-05 15:17:54 +0900

    feat : 일반회원 스케쥴 조회 컴포넌트 임시 구현

---
+++ src/components/PtSchedule/UserSchedule.tsx
@@ -0,0 +1,27 @@
+import { useUserPtDetail } from '@/hooks/useUserPtDetail';
+import { Wrapper } from './UserSchedule.styles';
+import { Card, Text } from '@chakra-ui/react';
+
+export const UserSchedule = () => {
+  const { ptDetail } = useUserPtDetail();
+
+  if (!ptDetail) {
+    return <Text>PT 일정 정보를 불러오는 중...</Text>;
+  }
+
+  return (
+    <Wrapper>
+      <Text>PT 일정</Text>
+      {ptDetail?.schedules.length === 0 ? (
+        <Text>등록된 PT 일정이 없습니다.</Text>
+      ) : (
+        ptDetail.schedules.map((schedule, index) => (
+          <Card key={index}>
+            <Text>{new Date(schedule.date).toLocaleDateString()}</Text>
+            <Text>{schedule.isCompleted ? '완료' : '미완료'}</Text>
+          </Card>
+        ))
+      )}
+    </Wrapper>
+  );
+};
