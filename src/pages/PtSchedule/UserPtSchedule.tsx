7d07bc750b02ae9719e6ad6b53bad818c6853a21
seorinnn <srgongju@naver.com>
2024-11-05 15:18:41 +0900

    feat : 내 PT 페이지 컴포넌트에 스케쥴 조회 컴포넌트 추가

--- src/pages/PtSchedule/UserPtSchedule.tsx
+++ src/pages/PtSchedule/UserPtSchedule.tsx
@@ -1,5 +1,12 @@
 import { TrainerProfile } from '@/components/PtSchedule/TrainerProfile';
+import { Wrapper } from './UserPtSchedule.styles';
+import { UserSchedule } from '@/components/PtSchedule/UserSchedule';
 
 export const UserPtSchedulePage = () => {
-  return <TrainerProfile />;
+  return (
+    <Wrapper>
+      <TrainerProfile />
+      <UserSchedule />
+    </Wrapper>
+  );
 };
