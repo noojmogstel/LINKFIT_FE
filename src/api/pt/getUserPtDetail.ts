7bc57dca289cda17bff6870aa1268c2597f47c5b
seorinnn <srgongju@naver.com>
2024-11-05 15:07:13 +0900

    refactor : 파일명(getUserPtDetail, useUserPtDetail) 변경 및 변수명(ptDetail) 수정

--- src/api/pt/getUserPtDetail.ts
+++ src/api/pt/getUserPtDetail.ts
@@ -1,6 +1,6 @@
 import { fetchAPI } from '..';
 
-export const getPtTrainerProfile = async () => {
+export const getUserPtDetail = async () => {
   return await fetchAPI({
     method: 'GET',
     endpoint: '/pt/user',
