ff8016f1b22c00e90d62ce30453f1a85620f2fa9
seorinnn <srgongju@naver.com>
2024-11-11 18:52:39 +0900

    style : 채팅하기 버튼 width, height prop으로 받도록 수정

--- src/components/Chat/ChattingButton.tsx
+++ src/components/Chat/ChattingButton.tsx
@@ -1,17 +1,22 @@
 import { useNavigate } from 'react-router-dom';
-
 import { getChattingRoomId } from '@/api/chat/getChattingRoom';
 import { RouterPath } from '@/routes/path';
-
 import { StyledChatButton } from './ChattingButton.styles';
 
-export const ChattingButton = ({ opponentId }: { opponentId: number }) => {
+export const ChattingButton = ({
+  opponentId,
+  width,
+  height,
+}: {
+  opponentId: number;
+  width?: string;
+  height?: string;
+}) => {
   const navigate = useNavigate();
 
   const navigateToChattingRoom = async () => {
     try {
       const response = await getChattingRoomId(opponentId.toString());
-
       navigate(`${RouterPath.chatting}/${response.roomId}`);
     } catch (error) {
       console.error('Error getting chat room ID:', error);
@@ -20,7 +25,12 @@ export const ChattingButton = ({ opponentId }: { opponentId: number }) => {
   };
 
   return (
-    <StyledChatButton type='button' onClick={navigateToChattingRoom}>
+    <StyledChatButton
+      type='button'
+      onClick={navigateToChattingRoom}
+      width={width}
+      height={height}
+    >
       채팅하기
     </StyledChatButton>
   );
