d74e63831d7d33b7bde8dd7c7245093b84eef5d0
seorinnn <srgongju@naver.com>
2024-11-12 13:45:03 +0900

    feat : 트레이너회원 마이페이지 컴포넌트에 TrainerGym 추가

--- src/pages/MyPage/TrainerMyPage.tsx
+++ src/pages/MyPage/TrainerMyPage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { TrainerMyPageProfile } from '@/components/MyPage/Profile/TrainerMyPageProfile';
 import { TrainerCareer } from '@/components/MyPage/TrainerCareer';
+import { TrainerGym } from '@/components/MyPage/TrainerGym';
 import { RouterPath } from '@/routes/path';
 
 import { StyledButton, Wrapper } from './TrainerMyPage.styles';
@@ -23,6 +24,7 @@ export const TrainerMyPage = () => {
       <StyledButton type='button' onClick={navigateToSelectGym}>
         헬스장 선택하기
       </StyledButton>
+      <TrainerGym />
       <StyledButton type='button' onClick={navigateToRegisterCareer}>
         경력 등록하기
       </StyledButton>
