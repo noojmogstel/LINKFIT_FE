97143967b7de5f95a78a76ecad9bf7e591f4a39d
seorinnn <srgongju@naver.com>
2024-11-12 18:58:06 +0900

    feat : ptDetail이 없는 경우 DefaultText 띄우도록 수정

--- src/components/PtSchedule/TrainerProfile.tsx
+++ src/components/PtSchedule/TrainerProfile.tsx
@@ -6,6 +6,7 @@ import { UserPtDetail } from '@/types';
 import { ChattingButton } from '../Chat/ChattingButton';
 import {
   StyledCardBody,
+  StyledDefaultText,
   StyledGymText,
   StyledNameText,
   StyledProfileImage,
@@ -15,26 +16,28 @@ import {
 export const TrainerProfile = () => {
   const { ptDetail } = usePtDetail<UserPtDetail>();
 
-  if (!ptDetail) {
-    return <p>프로필 정보를 불러오는 중...</p>;
-  }
-
   return (
     <Wrapper>
-      <Card>
-        <StyledCardBody>
-          <Flex>
-            <StyledProfileImage src={ptDetail.profileImageUrl} alt='프로필' />
-            <Flex direction='column' alignItems='flex-start'>
-              <StyledNameText mt='15px'>
-                {ptDetail.trainerName} 트레이너
-              </StyledNameText>
-              <StyledGymText>소속 : {ptDetail.gymName}</StyledGymText>
+      {!ptDetail ? (
+        <StyledDefaultText>
+          아직 PT 매칭이 이루어지지 않았습니다.
+        </StyledDefaultText>
+      ) : (
+        <Card>
+          <StyledCardBody>
+            <Flex>
+              <StyledProfileImage src={ptDetail.profileImageUrl} alt='프로필' />
+              <Flex direction='column' alignItems='flex-start'>
+                <StyledNameText mt='15px'>
+                  {ptDetail.trainerName} 트레이너
+                </StyledNameText>
+                <StyledGymText>소속 : {ptDetail.gymName}</StyledGymText>
+              </Flex>
             </Flex>
-          </Flex>
-          <ChattingButton opponentId={ptDetail.trainerId} />
-        </StyledCardBody>
-      </Card>
+            <ChattingButton opponentId={ptDetail.trainerId} />
+          </StyledCardBody>
+        </Card>
+      )}
     </Wrapper>
   );
 };
