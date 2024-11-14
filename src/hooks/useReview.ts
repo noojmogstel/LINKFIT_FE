ab13c26d85dbcdeeee2c33aed2be209127b20611
seorinnn <srgongju@naver.com>
2024-11-14 18:45:14 +0900

    feat : 리뷰 조회 로직 useReview 컴포넌트에 추가

--- src/hooks/useReview.ts
+++ src/hooks/useReview.ts
@@ -1,7 +1,32 @@
-import { postReview } from '@/api/review/postReview';
-import { Review } from '@/types';
+import { useEffect, useState } from 'react';
 
-export const useReview = () => {
+import { getMyReview, getReview } from '@/api/review/getReview';
+import { postReview } from '@/api/review/postReview';
+import { Review, ReviewResponse } from '@/types';
+
+export const useReview = (trainerId?: number) => {
+  const [reviews, setReviews] = useState<ReviewResponse[]>([]);
+
+  const fetchReviews = async (id: number) => {
+    try {
+      const data = await getReview(id);
+      setReviews(data);
+    } catch (error) {
+      console.error('리뷰 조회 실패:', error);
+      alert('리뷰 조회에 실패했습니다.');
+    }
+  };
+
+  const fetchMyReviews = async () => {
+    try {
+      const data = await getMyReview();
+      setReviews(data);
+    } catch (error) {
+      console.error('본인 리뷰 조회 실패:', error);
+      alert('리뷰 조회에 실패했습니다.');
+    }
+  };
+
   const submitReview = async (reviewData: Review) => {
     try {
       await postReview(reviewData);
@@ -12,5 +37,11 @@ export const useReview = () => {
     }
   };
 
-  return { submitReview };
+  useEffect(() => {
+    if (trainerId) {
+      fetchReviews(trainerId);
+    }
+  }, [trainerId]);
+
+  return { reviews, fetchReviews, fetchMyReviews, submitReview };
 };
